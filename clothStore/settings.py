868b3c7012f70780b327e737c1027e981f817741
munyua <munyuapeter07@gmail.com>
2020-10-15 16:22:40 +0300

    heroku configs

--- clothStore/settings.py
+++ clothStore/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'w$v3==cj1dn!nw^s!n^%d!va-3007+*gf@mi8gckfwbm1y3&tm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
