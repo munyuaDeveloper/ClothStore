b3e0648a89335b02ca8bc5841ee20142ce253603
munyua <munyuapeter07@gmail.com>
2020-11-11 18:16:05 +0300

    payment integration

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import SignUpView
+from .views import SignUpView, MyAccount, OrderDetails
+
 urlpatterns = [
-    path('register/', SignUpView.as_view(), name='register')
+    path('register/', SignUpView.as_view(), name='register'),
+    path('my-account/', MyAccount, name='my-account'),
+    path('my-account/order/<id>', OrderDetails, name='order_details')
 ]
--- account/views.py
+++ account/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+
+from orders.models import Order, OrderItem
 from .forms import CustomUserCreationForm
 
 
@@ -11,6 +15,24 @@ class SignUpView(CreateView):
     template_name = 'register.html'
 
 
-# class SignInView(LoginView):
-#     success_url = reverse_lazy('home')
-#     template_name = 'registration/login.html'
+def MyAccount(request):
+    customer_orders = Order.objects.filter(user=request.user)
+
+    context = {
+        'customer_orders': customer_orders
+    }
+    return render(request,
+                  'customer-orders.html', context
+                  )
+
+
+def OrderDetails(request, id):
+    order_details = Order.objects.filter(id=id)
+    order_items = OrderItem.objects.filter(order=id)
+    context = {
+        'order': order_details,
+        'order_items': order_items
+    }
+    return render(request,
+                  'customer-order.html', context
+                  )
